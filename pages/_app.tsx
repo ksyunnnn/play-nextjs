b942f4f9763bf3ffdb155061efb55cb943509658
NAME <EMAIL>
2021-06-08 22:31:29 +0900

    tailwind導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,21 @@ import { AppProps } from 'next/app';
 
 import Link from 'next/link';
 import { Provider as SessionProvider } from 'next-auth/client';
+import Head from 'next/head';
 import ContextProvider from '../context/Provider';
+
+import 'tailwindcss/tailwind.css';
 
 const App = ({ Component, pageProps, router }: AppProps) => {
   console.log({ pageProps, router });
   return (
     <SessionProvider session={pageProps.session}>
       <ContextProvider>
+        <Head>
+          <title>App</title>
+          <meta name="description" content="App" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <Component {...pageProps} />
         <footer>
           共通のフッター
