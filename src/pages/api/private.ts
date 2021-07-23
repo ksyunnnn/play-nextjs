65e5879e24784eae14e83e8c55dbac7a3f2eb505
ksyunnnn <is.syunsukekobashi@gmail.com>
2021-07-23 19:19:32 +0900

    fix

--- src/pages/api/private.ts
+++ src/pages/api/private.ts
@@ -4,15 +4,17 @@ import { getSession } from 'next-auth/client';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
 
+  console.log({ session, req, res });
+
   if (!session) {
     res.status(200).json({
       message: 'Need is login',
     });
+  } else {
+    res.status(200).json({
+      message: 'This is private information.',
+      name: 'John Doe',
+      email: 'ex@gmail.com',
+    });
   }
-
-  res.status(200).json({
-    message: 'This is private information.',
-    name: 'John Doe',
-    email: 'ex@gmail.com',
-  });
 };
