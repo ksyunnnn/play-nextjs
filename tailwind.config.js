f57418da34a6ea345380ea9d21d8eba6ed56d94e
ksyunnnn <is.syunsukekobashi@gmail.com>
2021-08-03 16:29:52 +0900

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // ←意味はわかっていない
+  purge: ['.src/pages/**/*.{js,ts,jsx,tsx}', '.src/components/**/*.{js,ts,jsx,tsx}'], // ←意味はわかっていない
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
